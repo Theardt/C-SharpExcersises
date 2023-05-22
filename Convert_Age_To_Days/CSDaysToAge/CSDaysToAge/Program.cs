c6169df6fb21d52eeda578601b8e5784d0f24994
Dean Clegg <deanclegg11@gmail.com>
2023-05-22 10:42:16 +0200

    Added user input

--- Convert_Age_To_Days/CSDaysToAge/CSDaysToAge/Program.cs
+++ Convert_Age_To_Days/CSDaysToAge/CSDaysToAge/Program.cs
@@ -1,11 +1,28 @@
 ﻿
 class AgetoDays
 {
-    private static readonly int age = 65;
+    private static int age;
+
     public static void Main()
     {
+        Console.WriteLine("Enter your age");
+        var inputvalue = Console.ReadLine();
 
-        Console.WriteLine("Hello, World!");
+        if (inputvalue != null)
+        {
+            int.TryParse(inputvalue, out age);
+            if (age == 0)
+            {
+                Console.WriteLine("Not valid number");
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not valid number");
+            return;
+        }
+
         var ageDays = CurrentAge(age);
         Console.WriteLine($"Your age is {ageDays}");
     }
