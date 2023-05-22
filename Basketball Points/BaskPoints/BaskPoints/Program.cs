39e1e53ab7cd9debf080587bd584521f7494afc8
Theardt <theardt.schoonbee@gmail.com>
2023-05-22 10:18:29 +0200

    TSBasketball Points exercise finished

--- Basketball Points/BaskPoints/BaskPoints/Program.cs
+++ Basketball Points/BaskPoints/BaskPoints/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+class BaskPoints
+{
+    private static int[] arrPoints = {1,1};
+    public static void Main()
+    {
+        int twoPointers;
+        int threePointers;
+
+        Console.WriteLine("How much two pointers did your team score?");
+        twoPointers = Convert.ToInt32(Console.ReadLine());
+        arrPoints[0] = twoPointers;
+
+        Console.WriteLine("How much three pointers did your team score?");
+        threePointers = Convert.ToInt32(Console.ReadLine());
+        arrPoints[1] = threePointers;
+
+        int Points = Pointers(arrPoints);
+        Console.WriteLine($"Your team's points scored is {Points}");
+    }
+
+    private static int Pointers(int[] points)
+    {
+        int Three = 3;
+        int Two = 2;
+        int Total = 0;
+        Total = points[0] * Two;
+        Total += points[1] * Three;
+
+        return Total;
+    }
+}
+
