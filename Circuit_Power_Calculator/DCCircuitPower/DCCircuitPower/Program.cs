b7e2e0830aa76befedcc217bb5221c1038ab211e
Dean Clegg <deanclegg11@gmail.com>
2023-05-22 11:59:55 +0200

    COmpleted Circuit power problem

---
+++ Circuit_Power_Calculator/DCCircuitPower/DCCircuitPower/Program.cs
@@ -0,0 +1,31 @@
+﻿public abstract class DCBasketBallPoints
+{
+    private static readonly int[] CircuitArray = new int[2];
+    private static int _voltage;
+    private static int _current;
+    public static void Main()
+    {
+
+        Console.WriteLine("What is your voltage?");
+        _voltage = Convert.ToInt32(Console.ReadLine());
+        CircuitArray[0] = _voltage;
+
+
+
+        Console.WriteLine("What is your current?");
+        _current = Convert.ToInt32(Console.ReadLine());
+        CircuitArray[1] = _current;
+
+        var circuitPower = BasketBallPoints(CircuitArray);
+
+        Console.WriteLine($"Your total circuit power is {circuitPower}");
+    }
+
+    private static int BasketBallPoints(int[] points)
+    {
+
+        var totalCircuitPower = points[0]*points[1];
+
+        return totalCircuitPower;
+    }
+}
