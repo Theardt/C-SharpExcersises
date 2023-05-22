8c3fd9acde4e44ca0e80e3dafece2abfb973a32e
Dean Clegg <deanclegg11@gmail.com>
2023-05-22 09:28:30 +0200

    Finished MinToSec Program

--- Convert_Min_To_Sec/MinToSec/MinToSec/Program.cs
+++ Convert_Min_To_Sec/MinToSec/MinToSec/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+class MinToSec
+{
+    private static int minutes = 3;
+    public static void Main()
+    {
+        int seconds = MintoSecFunc(minutes);
+        System.Console.WriteLine($"Numbr of seconds is {seconds}");
+    }
+
+    private static int MintoSecFunc(int mins)
+    {
+        int secs = mins * 60;
+        return secs;
+    } 
+}
