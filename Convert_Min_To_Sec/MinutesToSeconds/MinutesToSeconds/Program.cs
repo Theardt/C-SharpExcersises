f21d38ea0d05b701d37753e928fdb0cf319bd648
Theardt <theardt.schoonbee@gmail.com>
2023-05-22 09:29:19 +0200

    Minutes to seconds

---
+++ Convert_Min_To_Sec/MinutesToSeconds/MinutesToSeconds/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography.X509Certificates;
+
+class MinToSec
+{
+    private static int Minutes = 5;
+    public static void Main()
+    {
+        int Seconds = FuncMinToSec(Minutes);
+        System.Console.WriteLine($"Number of seconds is {Seconds}");    
+    }
+
+    private static int FuncMinToSec(int Mins)
+    { 
+        int secs = Mins * 60;
+        return secs; 
+    }
+}
