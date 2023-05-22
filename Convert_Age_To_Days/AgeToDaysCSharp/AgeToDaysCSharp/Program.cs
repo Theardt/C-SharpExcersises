8a949a7bb18362b82a9e364dc048358eaf00a2c6
Theardt <theardt.schoonbee@gmail.com>
2023-05-22 09:16:13 +0200

    Completed day to age converting

--- Convert_Age_To_Days/AgeToDaysCSharp/AgeToDaysCSharp/Program.cs
+++ Convert_Age_To_Days/AgeToDaysCSharp/AgeToDaysCSharp/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+class AgeToDays
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, what is your age?");
+        var ageText = 65;
+        var ageDays = CalcAge(ageText);
+        Console.WriteLine($"Your age is {ageDays}");
+    }
+
+    public static int CalcAge(int age)
+    {
+        var dayAgeint = age * 365;
+        return dayAgeint;
+    }
+
+}
